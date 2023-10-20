cb6e8dda3f37b677d98310bd5e53625b4e8fb7fe
pppig236 <priv@pppig236.com>
2023-10-19 21:43:46 -0400

    fix for autograder?

--- Labs/Lab 6/Code Files/park_placement.cpp
+++ Labs/Lab 6/Code Files/park_placement.cpp
@@ -2,7 +2,7 @@
 #include <cassert>
 #include <climits>
 #include <cmath>
-#include <cstdio>
+#include <iostream>
 
 // BE SURE TO RENAME TO park_placement.cpp BEFORE SUBMITTING
 
@@ -14,8 +14,7 @@ using namespace std;
 int NumIntersectPoints(int x1, int x2, int y1, int y2, int r1, int r2) {
   /*implement the function here*/
   int ret = 0;
-  int dis = 0;
-  int rSum = 0;
+  double dis = 0, rSum = 0;
   rSum = r1 + r2;
   dis = sqrt(pow((x1 - x2), 2) + pow((y1 - y2), 2));
   if (rSum > dis)
@@ -24,7 +23,7 @@ int NumIntersectPoints(int x1, int x2, int y1, int y2, int r1, int r2) {
     ret = 1;
   if (dis == 0) {
     if (r1 == r2)
-      ret = INT_MAX;
+      ret = -1;
     else
       ret = 0;
   }
@@ -33,16 +32,21 @@ int NumIntersectPoints(int x1, int x2, int y1, int y2, int r1, int r2) {
 
 int main() {
   // write your test cases here:
+  // assert(NumIntersectPoints(30, 30, 60, 30, 10, 5) ==
+  // 0);  // default one , no intersect
+  // assert(NumIntersectPoints(30, 30, 30, 30, 10, 5) ==
+  // 0);  // concentric , no intersect
+  // assert(NumIntersectPoints(30, 30, 30, 20, 5, 5) == 1);  // one intersect
+  // assert(NumIntersectPoints(30, 30, 16, 10, 5, 5) == 2);  // circle 1 is
+  // below circle2, two intersects assert(NumIntersectPoints(30, 30, 30, 20, 10,
+  // 5) == 2);  // circle 1 is above cicle 2, two intersects
+  // assert(NumIntersectPoints(30, 30, 30, 30, 30, 30) ==
+  // -1);  // same circle, inf intersects
+
   // call your function with specific inputs
   // check the output with assert statements
-  assert(NumIntersectPoints(30, 30, 60, 30, 10, 5) ==
-         0);  // default one , no intersect
-  assert(NumIntersectPoints(30, 30, 30, 30, 10, 5) ==
-         0);  // concentric , no intersect
-  assert(NumIntersectPoints(30, 30, 30, 20, 5, 5) == 1);  // one intersect
-  assert(NumIntersectPoints(30, 30, 16, 10, 5, 5) == 2);  // circle 1 is below circle2, two intersects
-  assert(NumIntersectPoints(30, 30, 30, 20, 10, 5) == 2);  // circle 1 is above cicle 2, two intersects
-  assert(NumIntersectPoints(30, 30, 30, 30, 30, 30) ==
-         INT_MAX);  // same circle, inf intersects
+  int x1, y1, r1, x2, y2, r2;
+  cin >> x1 >> x2 >> y1 >> y2 >> r1 >> r2;
+  cout << NumIntersectPoints(x1, y1, r1, x2, y2, r2);
   return 0;
 }
