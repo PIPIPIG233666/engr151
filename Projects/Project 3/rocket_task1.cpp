8f1210e39c7d669eeeff214e3a4adeb998958c56
pppig236 <priv@pppig236.com>
2023-11-06 14:29:12 -0500

    prj3: pt1: take 4?

--- Projects/Project 3/rocket_task1.cpp
+++ Projects/Project 3/rocket_task1.cpp
@@ -19,19 +19,17 @@ int main() {
   inF.clear();
   inF.seekg(0, ios::beg);
 
-  int n = 0;
   for (int i = 0; i < cnt / 4; i++) {
     inF >> T >> ve >> M_total >> M_payload;
     inF.close();
     M_n = M_total;
     M_fuel = M_n - M_payload;
     while (M_fuel > 0) {
-      n++;
       M_fuel = M_n - M_payload;
       M_n -= (T * dt) / ve;
       vn += (T / M_n) * dt;
     }
-    cout << vn << endl;
+    cout << vn;
   }
   return 0;
 }
