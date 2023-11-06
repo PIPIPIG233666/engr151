2fd538c00619e0f1d56ba016bfa9e96b37c04b32
pppig236 <priv@pppig236.com>
2023-11-06 00:14:52 -0500

    prj3: pt1: take 2?

--- Projects/Project 3/rocket_task1.cpp
+++ Projects/Project 3/rocket_task1.cpp
@@ -1,27 +1,25 @@
 #include <cmath>
 #include <fstream>
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <vector>
 using namespace std;
 int main() {
+  cout.precision(3);
   ifstream inF("init.txt");
-  const double /*e = 2.718, */dt = 0.0001;
-  int T, v0 = 0, ve;
-  double M_fuel, M_n, M_total, vn = v0, M_payload;
+  const double /*e = 2.718, */dt = 0.001;
+  int T, ve;
+  double M_fuel, M_n, M_total, vn = 0, M_payload;
   inF >> T >> ve >> M_total >> M_payload;
   inF.close();
   M_n = M_total;
   M_fuel = M_n - M_payload;
-  while (M_fuel >= 0) {
-    M_n = M_n - (T * dt) / ve;
+  while (M_fuel > 0) {
     M_fuel = M_n - M_payload;
-    vn = vn + (T / M_n) * dt;
+    M_n -= (T * dt) / ve;
+    vn += (T / M_n) * dt;
+    // cout << "Mass is: "<<M_n<<" Mass of fuel is: "<<M_fuel<<" v_f is: "<<vn<<endl;
   }
-  cout.precision(3);
-  cout << vn;
   // << endl << M_payload;
+  cout << vn;
   // M_payload = M_total * pow(1 / e, vn / ve);
   return 0;
 }
