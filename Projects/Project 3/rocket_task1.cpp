8903a46a0a73e97996b07cb2bab30834bfab2268
pppig236 <priv@pppig236.com>
2023-11-06 14:35:44 -0500

    prj3: pt1: take 5? - autograder sucks and is retarded

--- Projects/Project 3/rocket_task1.cpp
+++ Projects/Project 3/rocket_task1.cpp
@@ -4,33 +4,36 @@
 using namespace std;
 
 int main() {
-  cout.precision(3);
-  ifstream inF("init.txt");
-  const double dt = 0.001;
-  int T, ve;
-  double M_fuel, M_n, M_total, vn = 0, M_payload;
-  int cnt = 0;
-  string line;
-  while (getline(inF, line)) {
-    ++cnt;
-  }
+    cout.precision(3);
+    ifstream inF("init.txt");
+    const double dt = 0.001;
+    int T, ve;
+    double M_fuel, M_n, M_total, vn = 0, M_payload;
 
-  // Reset file pointer to beginning
-  inF.clear();
-  inF.seekg(0, ios::beg);
+    // Read input values line by line
+    string line;
+    if (getline(inF, line)) {
+        T = stoi(line);
+    }
+    if (getline(inF, line)) {
+        ve = stoi(line);
+    }
+    if (getline(inF, line)) {
+        M_total = stod(line);
+    }
+    if (getline(inF, line)) {
+        M_payload = stod(line);
+    }
 
-  for (int i = 0; i < cnt / 4; i++) {
-    inF >> T >> ve >> M_total >> M_payload;
     inF.close();
     M_n = M_total;
     M_fuel = M_n - M_payload;
     while (M_fuel > 0) {
-      M_fuel = M_n - M_payload;
-      M_n -= (T * dt) / ve;
-      vn += (T / M_n) * dt;
+        M_fuel = M_n - M_payload;
+        M_n -= (T * dt) / ve;
+        vn += (T / M_n) * dt;
     }
     cout << vn;
-  }
-  return 0;
+    return 0;
 }
 
