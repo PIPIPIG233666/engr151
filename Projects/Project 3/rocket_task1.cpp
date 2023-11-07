2561c58141da0d2d3f6a8a9cd4710c9e1b34a620
pppig236 <priv@pppig236.com>
2023-11-07 10:44:42 -0500

    pk3: pt1: comments

--- Projects/Project 3/rocket_task1.cpp
+++ Projects/Project 3/rocket_task1.cpp
@@ -4,32 +4,50 @@
 #include <vector>
 using namespace std;
 
-int main() {
-  cout.precision(3);
+/*
+ * calculates the V at n (final)
+ * returns double
+ */
+double getV_n();
+
+double getV_n() {
   ifstream inF("init.txt");
+  // dt is constant and should not change
   const double dt = 0.001;
-  int T, v_e;
-  double M_fuel, M_n, M_total, v_n = 0, M_payload;
+  int T;
+  double M_fuel, M_n, M_total, v_n = 0, M_payload, v_e;
   vector<double> lineVec;
   string line;
   while (getline(inF, line)) {
+    // append the lines to a vector
     lineVec.push_back(stod(line));
   }
+  // close the input file
+  inF.close();
 
+  // assign the vectors to corresponding variables
   T = lineVec[0];
   v_e = lineVec[1];
   M_total = lineVec[2];
   M_payload = lineVec[3];
 
-  inF.close();
+  // mass starts with fuel + rocket
   M_n = M_total;
+  // mass decreases by payload
   M_fuel = M_n - M_payload;
+  // keep the loop running until there is no fuel
   while (M_fuel > 0) {
     M_fuel = M_n - M_payload;
+    // formulas from the spec
     M_n -= (T * dt) / v_e;
     v_n += (T / M_n) * dt;
   }
-  cout << v_n;
+  return v_n;
+}
+
+int main() {
+  cout.precision(3);
+  cout << getV_n();
   return 0;
 }
 
