b0403aae8ce1f0c6b7e8fbd210c5b3d748bbd3c4
pppig236 <priv@pppig236.com>
2023-11-06 19:12:23 -0500

    prj3: pt2: tk1

--- Projects/Project 3/rocket_task1.cpp
+++ Projects/Project 3/rocket_task1.cpp
@@ -1,39 +1,35 @@
 #include <cmath>
 #include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    cout.precision(3);
-    ifstream inF("init.txt");
-    const double dt = 0.001;
-    int T, ve;
-    double M_fuel, M_n, M_total, vn = 0, M_payload;
+  cout.precision(3);
+  ifstream inF("init.txt");
+  const double dt = 0.001;
+  int T, ve;
+  double M_fuel, M_n, M_total, vn = 0, M_payload;
+  vector<double> lineVec;
+  string line;
+  while (getline(inF, line)) {
+    lineVec.push_back(stod(line));
+  }
 
-    // Read input values line by line
-    string line;
-    if (getline(inF, line)) {
-        T = stoi(line);
-    }
-    if (getline(inF, line)) {
-        ve = stoi(line);
-    }
-    if (getline(inF, line)) {
-        M_total = stod(line);
-    }
-    if (getline(inF, line)) {
-        M_payload = stod(line);
-    }
+  T = lineVec[0];
+  ve = lineVec[1];
+  M_total = lineVec[2];
+  M_payload = lineVec[3];
 
-    inF.close();
-    M_n = M_total;
+  inF.close();
+  M_n = M_total;
+  M_fuel = M_n - M_payload;
+  while (M_fuel > 0) {
     M_fuel = M_n - M_payload;
-    while (M_fuel > 0) {
-        M_fuel = M_n - M_payload;
-        M_n -= (T * dt) / ve;
-        vn += (T / M_n) * dt;
-    }
-    cout << vn;
-    return 0;
+    M_n -= (T * dt) / ve;
+    vn += (T / M_n) * dt;
+  }
+  cout << vn;
+  return 0;
 }
 
