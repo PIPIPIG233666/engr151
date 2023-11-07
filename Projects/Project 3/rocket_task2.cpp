7e3286f31b32c468c2b9d7159186da333ba4d3a9
pppig236 <umich.email@pppig236.com>
2023-11-07 12:24:10 -0500

    redo

--- Projects/Project 3/rocket_task2.cpp
+++ Projects/Project 3/rocket_task2.cpp
@@ -4,40 +4,28 @@
 #include <vector>
 using namespace std;
 
-/*
- * calculates the h at n (final)
- * returns double
- */
-double getH_n();
-
-double getH_n() {
+int main() {
+  cout.precision(3);
   ifstream inF("init.txt");
-  // dt and radius of earth are constant and should not change
   const double dt = 0.001, r_Earth = 6356;
   int T;
   double M_fuel, M_n, M_total, v_n = 0, M_payload, g_n, h_n = 0, v_esc, v_e;
   vector<double> lineVec;
   string line;
   while (getline(inF, line)) {
-    // append the lines to a vector
     lineVec.push_back(stod(line));
   }
-  // close the input file
   inF.close();
 
   for (int i = 0; i < (int)lineVec.size(); i += 4) {
-    // assign the vectors to corresponding variables if the init.txt has multiple inputs
     T = lineVec[i];
     v_e = lineVec[i + 1];
     M_total = lineVec[i + 2];
     M_payload = lineVec[i + 3];
 
-    // mass starts with fuel + rocket
     M_n = M_total;
-    // mass decreases by payload
     M_fuel = M_n - M_payload;
     bool exceed;
-    // keep the loop running until the velocity is no longer positive
     while (v_n >= 0) {
       M_fuel = M_n - M_payload;
       M_n -= (T * dt) / v_e;
@@ -48,15 +36,13 @@ double getH_n() {
         T = 0;
 
       exceed = v_esc <= v_n;
-      // return 1 early if exceeds
       if (exceed) {
         cout << "Escape velocity reached";
         return 1;
       }
 
-      // formulas from the spec
       h_n += v_n * dt;
-      g_n = 3.962e5 / pow(h_n + r_Earth, 2);
+      g_n = 3.962e5 / pow(h_n + r_Earth, 2);  // Recalculate g_n
       v_esc = sqrt(2 * g_n * (h_n + r_Earth));
 
       v_n += ((T / M_n) - g_n) * dt;
@@ -64,14 +50,9 @@ double getH_n() {
       // testing output
       // cout << "v_n is: " << v_n << "\th_n is: " << h_n << "\tg_n is: " << g_n << "\tv_esc is: " << v_esc << endl;
     }
+    cout << h_n;
     // cout << "Test " << (i / 4) + 1 << ": " << h_n << endl;
   }
-  return h_n;
-}
-
-int main() {
-  cout.precision(3);
-
-  cout << getH_n();
   return 0;
 }
+
--- Projects/Project 3/rocket_task3.cpp
+++ Projects/Project 3/rocket_task3.cpp
@@ -4,15 +4,9 @@
 #include <vector>
 using namespace std;
 
-/*
- * calculates the h at n (final)
- * returns double
- */
-double getH_n();
-
-double getH_n() {
+int main() {
+  cout.precision(3);
   ifstream inF("init.txt");
-  // more constats
   const double dt = 0.001, r_Earth = 6356, C_D = 0.500,
                A = 3.1415 * pow(6.6e-3 / 2, 2);
   int T;
@@ -20,27 +14,21 @@ double getH_n() {
                                roh_n;
   vector<double> lineVec;
   string line;
-  bool exceed;
   while (getline(inF, line)) {
-    // append the lines to a vector
     lineVec.push_back(stod(line));
   }
-  // close the input file
   inF.close();
 
   for (int i = 0; i < (int)lineVec.size(); i += 4) {
-    // assign the vectors to corresponding variables if the init.txt has multiple inputs
     T = lineVec[i];
     v_e = lineVec[i + 1];
     M_total = lineVec[i + 2];
     M_payload = lineVec[i + 3];
 
-    // mass starts with fuel + rocket
     M_n = M_total;
-    // mass decreases by payload
     M_fuel = M_n - M_payload;
     roh_n = 1.225e6 * exp(-h_n / 9);
-    // keep the loop running until the velocity is no longer positive
+    bool exceed;
     while (v_n >= 0) {
       M_fuel = M_n - M_payload;
       M_n -= (T * dt) / v_e;
@@ -50,11 +38,12 @@ double getH_n() {
       if (M_fuel <= 0)
         T = 0;
 
-      if (v_esc <= v_n) {
-        exceed = true;
-        break;
+      exceed = v_esc <= v_n;
+      if (exceed) {
+        cout << "Escape velocity reached";
+        return 1;
       }
-      // formulas from the spec
+
       h_n += v_n * dt;
       g_n = 3.962e5 / pow(h_n + r_Earth, 2);  // Recalculate g_n
       roh_n = 1.225e6 * exp(-h_n / 9);
@@ -65,21 +54,9 @@ double getH_n() {
       // testing output
       // cout << "v_n is: " << v_n << "\th_n is: " << h_n << "\tg_n is: " << g_n << "\tv_esc is: " << v_esc << endl;
     }
+    cout << h_n;
     // cout << "Test " << (i / 4) + 1 << ": " << h_n << endl;
   }
-  // return 1 early if exceeds
-  if (exceed) {
-    cout << "Escape velocity reached";
-    return 1;
-  }
-
-  return h_n;
-}
-
-int main() {
-  cout.precision(3);
-  cout << getH_n();
-  if (getH_n() == 1)
-    return 1;
   return 0;
 }
+
