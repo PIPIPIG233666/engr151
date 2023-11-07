da978c3d68a6a110fd8601059fafa2bb95f93905
pppig236 <priv@pppig236.com>
2023-11-06 23:09:44 -0500

    prj3: pt2: tk ?

--- Projects/Project 3/rocket_task2.cpp
+++ Projects/Project 3/rocket_task2.cpp
@@ -17,44 +17,43 @@ int main() {
   }
   inF.close();
 
-  T = lineVec[0];
-  v_e = lineVec[1];
-  M_total = lineVec[2];
-  M_payload = lineVec[3];
-//
-  // T = 30;
-  // v_e=4.5;
-  // M_total=2000;
-  // M_payload=50;
+  for (int i = 0; i < (int)lineVec.size(); i += 4) {
+    T = lineVec[i];
+    v_e = lineVec[i + 1];
+    M_total = lineVec[i + 2];
+    M_payload = lineVec[i + 3];
 
-  M_n = M_total;
-  M_fuel = M_n - M_payload;
-  bool exceed;
-  while (v_n >= 0) {
+    M_n = M_total;
     M_fuel = M_n - M_payload;
-    M_n -= (T * dt) / v_e;
+    bool exceed;
+    while (v_n >= 0) {
+      M_fuel = M_n - M_payload;
+      M_n -= (T * dt) / v_e;
 
-    // out of fuel
-    // thrust is zero
-    if (M_fuel <= 0)
-      T = 0;
+      // out of fuel
+      // thrust is zero
+      if (M_fuel <= 0)
+        T = 0;
 
-    exceed = v_esc <= v_n;
-    if (exceed) {
-      cout << "Escape velocity reached";
-      return 1;
+      exceed = v_esc <= v_n;
+      if (exceed) {
+        cout << "Escape velocity reached";
+        return 1;
+      }
+
+      h_n += v_n * dt;
+      g_n = 3.962e5 / pow(h_n + r_Earth, 2);  // Recalculate g_n
+      v_esc = sqrt(2 * g_n * (h_n + r_Earth));
+
+      v_n += ((T / M_n) - g_n) * dt;
+
+      // testing output
+      // cout << "v_n is: " << v_n << "\th_n is: " << h_n << "\tg_n is: " << g_n << "\tv_esc is: " << v_esc << endl;
     }
-
-    h_n += v_n * dt;
-    g_n = 3.962e5 / pow(h_n + r_Earth, 2);  // Recalculate g_n
-    v_esc = sqrt(2 * g_n * (h_n + r_Earth));
-
-    v_n += ((T / M_n) - g_n) * dt;
-
-    // testing output
-    // cout << "v_n is: " << v_n << "\th_n is: " << h_n << "\tg_n is: " << g_n << "\tv_esc is: " << v_esc << endl;
+    cout << "Test " << (i / 4) + 1 << ": " << h_n << endl;
+    v_n = 0;  // Reset velocity for next test case
+    h_n = 0;  // Reset height for next test case
   }
-  cout << h_n;
   return 0;
 }
 
