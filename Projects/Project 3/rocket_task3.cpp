b5712ec0b924e6af011058ee18efe8fb6f0092ac
pppig236 <priv@pppig236.com>
2023-11-08 20:39:56 -0500

    refactor 3

--- Projects/Project 3/rocket_task3.cpp
+++ Projects/Project 3/rocket_task3.cpp
@@ -3,16 +3,47 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+double getH_n(double T, double v_e, double M_total, double M_payload) {
+  // constants
+  const double dt = 0.001, r_Earth = 6356, C_D = 0.500,
+               A = 3.1415 * pow(6.6e-3 / 2, 2);
+  double M_fuel = 0, M_n = 0, v_n = 0, g_n = 0, h_n = 0, v_esc = 0, roh_n = 0;
+  // mass starts with fuel
+  M_n = M_total;
+  // mass decreases by payload
+  M_fuel = M_n - M_payload;
+  roh_n = 1.225e6 * exp(-h_n / 9);
+  bool exceed;
+  while (v_n >= 0) {
+    M_fuel = M_n - M_payload;
+    M_n -= (T * dt) / v_e;
+
+    // out of fuel
+    // thrust is zero
+    if (M_fuel <= 0)
+      T = 0;
+
+    // return early if escape velocity is exceeded
+    exceed = v_esc < v_n;
+    if (exceed) {
+      cout << "Escape velocity reached";
+      return 1;
+    }
+
+    // formulas from the spec
+    h_n += v_n * dt;
+    g_n = 3.962e5 / pow(h_n + r_Earth, 2);
+    roh_n = 1.225e6 * exp(-h_n / 9);
+    v_esc = sqrt(2 * g_n * (h_n + r_Earth));
+    double airR = 1 / (2 * M_n) * roh_n * C_D * A * pow(v_n, 2);
+    v_n += ((T / M_n) - g_n - airR) * dt;
+  }
+  return h_n;
+}
 
 int main() {
   cout.precision(3);
   ifstream inF("init.txt");
-  // constants
-  const double dt = 0.001, r_Earth = 6356, C_D = 0.500,
-               A = 3.1415 * pow(6.6e-3 / 2, 2);
-  int T;
-  double M_fuel = 0, M_n=0, M_total=0, v_n = 0, M_payload=0, g_n=0, h_n = 0, v_esc=0, v_e=0,
-                               roh_n=0;
   vector<double> lineVec;
   string line;
   while (getline(inF, line)) {
@@ -22,49 +53,15 @@ int main() {
   // close the input file
   inF.close();
 
-  for (int i = 0; i < (int)lineVec.size(); i += 4) {
-    // assign the vectors to corresponding variables
-    T = lineVec[i];
-    v_e = lineVec[i + 1];
-    M_total = lineVec[i + 2];
-    M_payload = lineVec[i + 3];
+  // for (int i = 0; i < (int)lineVec.size(); i += 4) {
+  // double o =
+  // getH_n(lineVec[i], lineVec[i + 1], lineVec[i + 2], lineVec[i + 3]);
+  // cout << "Test " << (i / 4) + 1 << ": " << o << endl;
+  // }
+  int i = 0;
+  double o = getH_n(lineVec[i], lineVec[i + 1], lineVec[i + 2], lineVec[i + 3]);
+  cout << o;
 
-    // mass starts with fuel 
-    M_n = M_total;
-    // mass decreases by payload
-    M_fuel = M_n - M_payload;
-    roh_n = 1.225e6 * exp(-h_n / 9);
-    bool exceed;
-    while (v_n >= 0) {
-      M_fuel = M_n - M_payload;
-      M_n -= (T * dt) / v_e;
-
-      // out of fuel
-      // thrust is zero
-      if (M_fuel <= 0)
-        T = 0;
-
-      // return early if escape velocity is exceeded
-      exceed = v_esc < v_n;
-      if (exceed) {
-        cout << "Escape velocity reached";
-        return 1;
-      }
-
-      // formulas from the spec
-      h_n += v_n * dt;
-      g_n = 3.962e5 / pow(h_n + r_Earth, 2);
-      roh_n = 1.225e6 * exp(-h_n / 9);
-      v_esc = sqrt(2 * g_n * (h_n + r_Earth));
-      double airR = 1 / (2 * M_n) * roh_n * C_D * A * pow(v_n, 2);
-      v_n += ((T / M_n) - g_n - airR) * dt;
-
-      // testing output
-      // cout << "v_n is: " << v_n << "\th_n is: " << h_n << "\tg_n is: " << g_n << "\tv_esc is: " << v_esc << endl;
-    }
-    cout << h_n;
-    // cout << "Test " << (i / 4) + 1 << ": " << h_n << endl;
-  }
   return 0;
 }
 
