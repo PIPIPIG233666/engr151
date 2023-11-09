af004b9ce60c7eed0b3d9a263ba17a1fa840a071
pppig236 <priv@pppig236.com>
2023-11-08 21:14:35 -0500

    prj3: pt4: yay

--- Projects/Project 3/rocket_task4.cpp
+++ Projects/Project 3/rocket_task4.cpp
@@ -3,31 +3,11 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-double getH_n(double m);
-double getH_n(double m) {
-  ifstream inF("init_opt.txt");
+double getH_n(double T, double v_e, double M_total, double M_payload) {
   // constants
   const double dt = 0.001, r_Earth = 6356, C_D = 0.500,
                A = 3.1415 * pow(6.6e-3 / 2, 2);
-  int T;
-  double M_fuel = 0, M_n = 0, M_total = 0, v_n = 0, M_payload = 0, g_n = 0,
-         h_n = 0, v_esc = 0, v_e = 0, roh_n = 0;
-  vector<double> lineVec;
-  string line;
-  while (getline(inF, line)) {
-    // append the lines to a vector
-    lineVec.push_back(stod(line));
-  }
-  // close the input file
-  inF.close();
-
-  // assign the vectors to corresponding variables
-  T = lineVec[0];
-  v_e = lineVec[1];
-  M_total = 2000;
-  M_payload = lineVec[2];
-
+  double M_fuel = 0, M_n = 0, v_n = 0, g_n = 0, h_n = 0, v_esc = 0, roh_n = 0;
   // mass starts with fuel
   M_n = M_total;
   // mass decreases by payload
@@ -46,7 +26,6 @@ double getH_n(double m) {
     // return early if escape velocity is exceeded
     exceed = v_esc < v_n;
     if (exceed) {
-      cout << "Escape velocity reached";
       return 1;
     }
 
@@ -57,36 +36,53 @@ double getH_n(double m) {
     v_esc = sqrt(2 * g_n * (h_n + r_Earth));
     double airR = 1 / (2 * M_n) * roh_n * C_D * A * pow(v_n, 2);
     v_n += ((T / M_n) - g_n - airR) * dt;
-
-    // testing output
-    // cout << "v_n is: " << v_n << "\th_n is: " << h_n << "\tg_n is: " << g_n
-    // << "\tv_esc is: " << v_esc << endl;
   }
-  // cout << "Test " << (i / 4) + 1 << ": " << h_n << endl;
   return h_n;
-}
-
-double getFuel() {
-  cout <<"test";
-  const double M_max = 25 / 0.000981, h_tar = 2000;
-  double M_low = 0, M_high = M_max, M_fuel, h_n;
-  M_fuel = 0.5 * (M_low + M_high);
-  h_n = getH_n(M_fuel);
-  cout << h_n;
-  while (abs(h_n - h_tar) > 5) {
-    if (h_n > h_tar)
-      M_high = M_fuel;
-    else
-      M_low = M_fuel;
-  }
-
-  cout << " fuel\t " << M_fuel << endl;
-  return M_fuel;
 }
 
 int main() {
   cout.precision(3);
-  getFuel();
+  ifstream inF("init_opt.txt");
+  vector<double> lineVec;
+  string line;
+  while (getline(inF, line)) {
+    // append the lines to a vector
+    lineVec.push_back(stod(line));
+  }
+  // close the input file
+  inF.close();
+
+  // for (int i = 0; i < (int)lineVec.size(); i += 4) {
+  // double o =
+  // getH_n(lineVec[i], lineVec[i + 1], lineVec[i + 2], lineVec[i + 3]);
+  // cout << "Test " << (i / 4) + 1 << ": " << o << endl;
+  // }
+  int i = 0;
+  const double h_tar = 2000;
+  double M_fuel_max = 35 / 0.00981, M_fuel_min = 0,
+         M_fuel = .5 * (M_fuel_min + M_fuel_max), low = M_fuel_min,
+         high = M_fuel_max;
+  double h = getH_n(lineVec[i], lineVec[i + 1], M_fuel, lineVec[i + 2]);
+  if (h < h_tar) {
+    cout << "Rocket too heavy";
+    return 1;
+  }
+  if (h == 1) {
+    cout << "Escape velocity reached";
+    return 1;
+  }
+  while (abs(h - h_tar) > 5) {
+    if (h > h_tar)
+      high = M_fuel;
+    else
+      low = M_fuel;
+    M_fuel = .5 * (high + low);
+
+    h = getH_n(lineVec[i], lineVec[i + 1], M_fuel, lineVec[i + 2]);
+    // cout << h<<endl;
+  }
+  cout << M_fuel << endl;
+
   return 0;
 }
 
