20362344d15c3621c70f7ec44ea04ab3d6ca5c7c
pppig236 <priv@pppig236.com>
2023-11-27 20:57:26 -0500

    prj4: final tk

--- Projects/Project 4/starter-code/fluorine.hpp
+++ Projects/Project 4/starter-code/fluorine.hpp
@@ -1,19 +1,19 @@
 // TODO: Implement derived Fluorine18 class
-#include <cmath>
 #include "radionuc.hpp"
 
 class Fluorine18 : public radioNuclide {
  public:
+  // inhenrit the public class members
   using radioNuclide::radioNuclide;
+  // default constructors
   Fluorine18();
   Fluorine18(const int numNuc);
   double m_lamda = log(2.0) / (109.771 / 60.0);
-
- private:
 };
 
-Fluorine18::Fluorine18(){};
+Fluorine18::Fluorine18(){/*do nothing*/};
 
-Fluorine18::Fluorine18(const int numNuc){
-    radioNuclide(numNuc, Fluorine18::m_lamda);
+// assign the input to the inhenrited class lamda
+Fluorine18::Fluorine18(const int numNuc) {
+  radioNuclide(numNuc, Fluorine18::m_lamda);
 }
--- Projects/Project 4/starter-code/radionuc.hpp
+++ Projects/Project 4/starter-code/radionuc.hpp
@@ -1,70 +1,87 @@
 #pragma once
-#include <iostream>
 #include <cmath>
+#include <iostream>
 
 using namespace std;
 
 // TODO: Declare and implement radioNuclide class
 class radioNuclide {
  private:
+  // private members
   int m_numNuc;
   double m_lamda;
 
  public:
-  radioNuclide();
-  radioNuclide(const double lamda, const int numNuc);
-  int Count();
-  int Decay(const double t_int);
-  void Add(const int numNuc);
-  void Zero();
-  void operator+=(const int numNuc);
+  // public members
+  // default constructors
+  radioNuclide();                                      // empty
+  radioNuclide(const double lamda, const int numNuc);  // two arguments
+
+  int Count();  // getter function for getting the private var m_numNuc
+  int Decay(
+      const double
+          t_int);  // decaying function for updating the population by subtracting
+  void Add(
+      const int
+          numNuc);  // adding function for adding negative values (decaying)
+  void Zero();      // setter function for zeroing out population
+
+  void operator+=(
+      const int numNuc);  // overload function of +=, is equivlant to Add()
 };
 
-radioNuclide::radioNuclide() {}
+radioNuclide::radioNuclide() { /*do nothing*/
+}
 
 radioNuclide::radioNuclide(const double lamda, const int numNuc) {
+  // assign values to private vars
   m_lamda = lamda;
   m_numNuc = numNuc;
 }
 
 int radioNuclide::Count() {
+  // return the private population
   return m_numNuc;
 };
 
 int radioNuclide::Decay(const double t_int) {
   // Compute the probability for a single nucleus to decay
-  double decayP = 1 - exp(-m_lamda * t_int);
+  double decayP = m_lamda * t_int;
 
-  // Cnt the number of nuclei that decay
+  // Count the number of nuclei that decay
   int decayedCnt = 0;
   for (int i = 0; i < m_numNuc; i++) {
     // Generate a random number between 0 and 1
     double randNum = rand() / (double)RAND_MAX;
 
     // Compare with the decay probability
-    if (randNum < decayP) {
+    if (randNum < decayP)
       decayedCnt++;
-    }
   }
 
   // Update the population size
   Add(-decayedCnt);
 
+  // return the number of nuclei that decays
   return decayedCnt;
 }
 
 void radioNuclide::Add(const int numNuc) {
+  // Adding input to the private population
   m_numNuc += numNuc;
 };
 
 void radioNuclide::Zero() {
+  // Setting the private population to zero
   m_numNuc = 0;
 };
 
 void radioNuclide::operator+=(const int numNuc) {
+  // Call Add() since it is the same function
   Add(numNuc);
 };
 
+// overload of << operator to output the getter
 ostream& operator<<(ostream& os, radioNuclide& rNuc) {
   os << rNuc.Count();
   return os;
